070f47ba64255e1fd857d8203df05c4dc69d7de0
pijoules <lchan1994@yahoo.com>
2016-02-07 18:13:33 -0500

    gotta increase that github score

--- tuning.py
+++ tuning.py
@@ -8,54 +8,6 @@ from genetic import Genetic
 from pid import PID
 
 
-class Gains(object):
-    __slots__ = ("_p", "_i", "_d")
-
-    """
-    I'm stupid for this. The main reason why I have this class is just
-    because you cannot hash a list, but you can hash a tuple.
-    The hashing is required in genetic.py.
-    """
-
-    def __init__(self, p, i, d):
-        self._p = p
-        self._i = i
-        self._d = d
-
-    def mutate(self, p, i, d):
-        """Just a setter method for the gains."""
-        self._p = p
-        self._i = i
-        self._d = d
-
-    @classmethod
-    def from_parents(cls, male, female):
-        """Breed a child from parents."""
-        return Gains(male.P, female.I, female.D)
-
-    @property
-    def P(self):
-        return self._p
-
-    @property
-    def I(self):
-        return self._i
-
-    @property
-    def D(self):
-        return self._d
-
-    @property
-    def gains(self):
-        return (self._p, self._i, self._d)
-
-    def __eq__(self, other):
-        return self.gains == other.gains
-
-    def __hash__(self):
-        return hash((self._p, self._i, self._d))
-
-
 class GeneticTuner(Genetic):
     """
     Find the gain values of a PID controller using a
@@ -93,9 +45,6 @@ class GeneticTuner(Genetic):
 
     def _create_individual(self):
         """Individual is just gains."""
-        #return Gains(uniform(self._min_p, self._max_p),
-        #             uniform(self._min_i, self._max_i),
-        #             uniform(self._min_d, self._max_d))
         return [uniform(self._min_p, self._max_p),
                 uniform(self._min_i, self._max_i),
                 uniform(self._min_d, self._max_d)]
@@ -108,9 +57,6 @@ class GeneticTuner(Genetic):
         dt = self._sample_period
         tau = self._tau
 
-        #pid = PID(Kp=individual.P,
-        #          Ki=individual.I,
-        #          Kd=individual.D)
         pid = PID(Kp=individual[0],
                   Ki=individual[1],
                   Kd=individual[2])
@@ -135,22 +81,15 @@ class GeneticTuner(Genetic):
 
     def _mutate_individual(self, individual):
         """Randomly change any of the gains."""
-        #Kp, Ki, Kd = individual.gains
-        Kp, Ki, Kd = individual
         if random() < 0.333:
-            Kp = uniform(self._min_p, self._max_p)
+            individual[0] = uniform(self._min_p, self._max_p)
         if random() < 0.333:
-            Ki = uniform(self._min_i, self._max_i)
+            individual[1] = uniform(self._min_i, self._max_i)
         if random() < 0.333:
-            Kd = uniform(self._min_d, self._max_d)
-        #individual.mutate(Kp, Ki, Kd)
-        individual[0] = Kp
-        individual[1] = Ki
-        individual[2] = Kd
+            individual[2] = uniform(self._min_d, self._max_d)
 
     def _breed(self, male, female):
         """Combine the traits of 2 different gains."""
-        #return Gains.from_parents(male, female)
         return [male[0], female[1], female[2]]
 
     def find_gains(self, target, iterations=100, retain=0.2,
